2eee037f570838c4407d449dbd62c9defc145aeb
Willian <willian.amorim1@outlook.com>
2023-04-25 17:15:12 -0300

    adicionado função para criar um produto e retornar o objeto completo do produto com id, name e amount

---
+++ src/Controllers/product.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import ProductService from '../Services/product';
+
+const create = async (req: Request, res: Response) => {
+  const { name, amount } = req.body;
+  const product = await ProductService.create(name, amount);
+
+  return res.status(200).json(product);
+};
+
+export default {
+  create,
+};
