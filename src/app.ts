b8d6236ea404e65fee946e046e36be41507bf282
Willian <willian.amorim1@outlook.com>
2023-04-25 17:15:45 -0300

    importado e utilização função para a rota post de /products

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import ProductController from './Controllers/product';
+
 const app = express();
 
 app.use(express.json());
 
-app.post('/products');
+app.post('/products', ProductController.create);
 
 export default app;
