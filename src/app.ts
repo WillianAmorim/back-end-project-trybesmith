b7afd24af052492b17329ee134486621edb2e248
Willian <willian.amorim1@outlook.com>
2023-04-26 17:29:08 -0300

    adicionado rota para adição de usuários

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import ProductController from './controllers/product';
+import UserController from './controllers/user';
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(express.json());
 app.post('/products', ProductController.create);
 
 app.get('/products', ProductController.listAll);
+
+app.post('/users', UserController.create);
 
 export default app;
