10cad043107178880f72124ca1698bd5f62fb397
Willian <willian.amorim1@outlook.com>
2023-04-26 19:39:26 -0300

    alterado o recebimento do parâmetro para newUser, e alterado a tipagem para um type User

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,20 +1,18 @@
 import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
+import { User } from '../Types/user';
 
-const create = async (username:string, vocation:string, level:number, password:string) => {
+const create = async (newUser:User) => {
   const sql = `insert into Trybesmith.users
     (username, vocation, level, password) values (?, ?, ?, ?)
   `;
   const [{ insertId }] = await connection
-    .execute<ResultSetHeader>(sql, [username, vocation, level, password]);
+    .execute<ResultSetHeader>(
+    sql,
+    [newUser.username, newUser.vocation, newUser.level, newUser.password],
+  );
   
-  return {
-    id: insertId,
-    username, 
-    vocation, 
-    level,
-    password,
-  };
+  return { id: insertId, ...newUser };
 };
 
 export default {
