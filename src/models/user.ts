d171a8359f418bc1e53230312e4e9e6e7d105075
Willian <willian.amorim1@outlook.com>
2023-04-28 14:06:10 -0300

    adicionado função de verificação para username e outra função para password

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,7 @@
-import { ResultSetHeader } from 'mysql2';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import connection from './connection';
 import { User } from '../Types/user';
+import { Login } from '../Types/login';
 
 const create = async (newUser:User):Promise<User> => {
   const sql = `insert into Trybesmith.users
@@ -15,6 +16,26 @@ const create = async (newUser:User):Promise<User> => {
   return { id: insertId, ...newUser };
 };
 
+const getName = async (username: string): Promise<Login | undefined> => {
+  const query = 'SELECT * from Trybesmith.users WHERE username = ?';
+  const [[userName]] = await connection.execute<RowDataPacket[]>(query, [username]);
+
+  console.log(userName);
+
+  return userName as Login | undefined;
+};
+
+const getPassword = async (password: string): Promise<Login | undefined> => {
+  const query = 'SELECT * from Trybesmith.users WHERE password = ?';
+  const [[userPassword]] = await connection.execute<RowDataPacket[]>(query, [password]);
+
+  console.log(userPassword);
+
+  return userPassword as Login | undefined;
+};
+
 export default {
   create,
+  getName,
+  getPassword,
 };
